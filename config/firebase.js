3a329e92107ea521e8e04c6765ebe7884a0cc24f
DANU DWIKI LAKSANA <ddlgamers14@gmail.com>
2024-05-02 13:57:19 +0700

    env

--- config/firebase.js
+++ config/firebase.js
@@ -5,26 +5,15 @@ const { getDatabase } = require('firebase/database');
 
 require('dotenv').config();
 
-// const firebaseConfig = {
-//     apiKey: process.env.apiKey,
-//     authDomain: process.env.authDomain,
-//     databaseURL: process.env.databaseURL,
-//     projectId: process.env.projectId,
-//     storageBucket: process.env.storageBucket,
-//     messagingSenderId: process.env.messagingSenderId,
-//     appId: process.env.appId,
-//     measurementId: process.env.measurementId
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAEFT9PURvTyOBTtWxzg_UFhNGlsBAkVyI",
-    authDomain: "birdfarm-36e07.firebaseapp.com",
-    databaseURL: "https://birdfarm-36e07-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "birdfarm-36e07",
-    storageBucket: "birdfarm-36e07.appspot.com",
-    messagingSenderId: "880525860323",
-    appId: "1:880525860323:web:87a94e0e0b73f7021a54d6",
-    measurementId: "G-6PYCYWVM46"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 };
 
 // init firebase
